873fd405aa1f09d550703af16da811b2f48943a6
TaylorPlovanich <taylor.plovanich@umconnect.umt.edu>
2025-02-06 12:03:22 -0700

    HW 4

--- HW-4/script.js
+++ HW-4/script.js
@@ -4,19 +4,19 @@ function playGame() {
     let imagePath = "";
 
     if (choice === "look around") {
-        storyText = "You see a dusty lantern and some broken crates.";
+        storyText = "You see a dusty lantern and some broken crates. What do you do next? (Type 'open door', 'pick up the lantern', or 'inspect the crates')";
         imagePath = "images/crates.jpg";
     } else if (choice === "open door") {
-        storyText = "You open the door and see a foggy field.";
+        storyText = "You open the door and see a foggy field. What do you do next? (Type 'walk towards the field' or 'return inside')";
         imagePath = "images/foggyfield.jpg";
     } else if (choice === "pick up the lantern") {
-        storyText = "The lantern is broken. It does nothing.";
+        storyText = "The lantern is broken. It does nothing. What do you do next? (Type 'look around' or 'inspect the crates')";
         imagePath = "images/lantern.jpg";
     } else if (choice === "inspect the crates") {
-        storyText = "You find an old book.";
+        storyText = "You find an old book. What do you do next? (Type 'pick up the book' or 'look around')";
         imagePath = "images/book.jpg";
     } else if (choice === "pick up the book") {
-        storyText = "Inside the book, you find a small radio. You call for help and are saved!";
+        storyText = "Inside the book, you find a small radio. You call for help and are saved! You win!";
         imagePath = "images/radio.jpg";
     } else if (choice === "walk towards the field") {
         storyText = "You walk into the field and fall into a trap. Game over.";
@@ -25,13 +25,14 @@ function playGame() {
         storyText = "You return inside, but a demon gets in and attacks you. Game over.";
         imagePath = "images/demon.jpg";
     } else {
-        storyText = "Invalid choice. Try again.";
+        storyText = "Invalid choice. Please try again.";
         imagePath = "images/error.jpg";
     }
 
     document.getElementById("story").innerText = storyText;
     document.getElementById("image").src = imagePath;
 }
+
 
 function restartGame() {
     document.getElementById("story").innerText = "You wake up in an abandoned barn. What do you do?";

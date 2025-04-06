1652f4c9e2e119785a4a0b299eabc8d2ddd5f6f6
TaylorPlovanich <taylor.plovanich@umconnect.umt.edu>
2025-04-06 11:44:15 -0600

    Uploading HW 12

--- HW-12/game.js
+++ HW-12/game.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-let player;
 let factoryObjects = [];
 let collectibles = [];
 let score = 0;
@@ -16,38 +15,28 @@ class Player {
     this.speed = 5;
   }
 
-  draw(ctx) {
+  move(dx, dy) {
+    const newX = this.x + dx;
+    const newY = this.y + dy;
+
+    // Check collision with factory objects
+    const willCollide = factoryObjects.some(obj =>
+      isColliding({ x: newX, y: newY, width: this.width, height: this.height }, obj)
+    );
+
+    if (!willCollide) {
+      this.x = newX;
+      this.y = newY;
+    }
+  }
+
+  draw() {
     ctx.fillStyle = "blue";
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
-
-  moveUp() {
-    if (!this.collides(0, -this.speed)) this.y -= this.speed;
-  }
-
-  moveDown() {
-    if (!this.collides(0, this.speed)) this.y += this.speed;
-  }
-
-  moveLeft() {
-    if (!this.collides(-this.speed, 0)) this.x -= this.speed;
-  }
-
-  moveRight() {
-    if (!this.collides(this.speed, 0)) this.x += this.speed;
-  }
-
-  collides(dx, dy) {
-    return factoryObjects.some(obj =>
-      this.x + dx < obj.x + obj.width &&
-      this.x + dx + this.width > obj.x &&
-      this.y + dy < obj.y + obj.height &&
-      this.y + dy + this.height > obj.y
-    );
-  }
 }
 
-// Factory object class
+// Obstacle class
 class FactoryObject {
   constructor(name, x, y, width, height) {
     this.name = name;
@@ -57,7 +46,7 @@ class FactoryObject {
     this.height = height;
   }
 
-  draw(ctx) {
+  draw() {
     ctx.fillStyle = "gray";
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
@@ -74,52 +63,57 @@ class Collectible {
     this.collected = false;
   }
 
-  draw(ctx) {
+  draw() {
     if (!this.collected) {
       ctx.fillStyle = "gold";
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
   }
 
-  collect(player) {
-    if (!this.collected &&
-      player.x < this.x + this.width &&
-      player.x + player.width > this.x &&
-      player.y < this.y + this.height &&
-      player.y + player.height > this.y) {
+  checkCollection(player) {
+    if (!this.collected && isColliding(this, player)) {
       this.collected = true;
-      return true;
+      score++;
+      console.log("Score: " + score);
     }
-    return false;
   }
 }
 
-// Load JSON files for objects and collectibles
-fetch('factoryObjects.json')
-  .then(response => response.json())
+// Collision detection
+function isColliding(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
+}
+
+const player = new Player(50, 50, 40, 40);
+
+// Load JSON files
+fetch("factoryObjects.json")
+  .then(res => res.json())
   .then(data => {
     data.forEach(item => {
       factoryObjects.push(new FactoryObject(item.name, item.x, item.y, item.width, item.height));
     });
   });
 
-fetch('collectibles.json')
-  .then(response => response.json())
+fetch("collectibles.json")
+  .then(res => res.json())
   .then(data => {
     data.forEach(item => {
       collectibles.push(new Collectible(item.name, item.x, item.y, item.width, item.height));
     });
   });
 
-// Initialize player
-player = new Player(100, 100, 40, 40);
-
-// Handle player movement with arrow keys
-document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowUp") player.moveUp();
-  if (e.key === "ArrowDown") player.moveDown();
-  if (e.key === "ArrowLeft") player.moveLeft();
-  if (e.key === "ArrowRight") player.moveRight();
+// Keyboard controls
+document.addEventListener("keydown", e => {
+  if (e.key === "ArrowUp") player.move(0, -player.speed);
+  if (e.key === "ArrowDown") player.move(0, player.speed);
+  if (e.key === "ArrowLeft") player.move(-player.speed, 0);
+  if (e.key === "ArrowRight") player.move(player.speed, 0);
 });
 
 // Game loop
@@ -127,19 +121,16 @@ function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // Draw factory objects
-  factoryObjects.forEach(object => object.draw(ctx));
+  factoryObjects.forEach(obj => obj.draw());
 
   // Draw and check collectibles
-  collectibles.forEach(collectible => {
-    collectible.draw(ctx);
-    if (collectible.collect(player)) {
-      score++;
-      console.log("Score: " + score);
-    }
+  collectibles.forEach(item => {
+    item.draw();
+    item.checkCollection(player);
   });
 
   // Draw player
-  player.draw(ctx);
+  player.draw();
 
   requestAnimationFrame(gameLoop);
 }

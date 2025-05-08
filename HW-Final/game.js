1e42e601a33e05528517e04762c8f3b6e66046af
TaylorPlovanich <taylor.plovanich@umconnect.umt.edu>
2025-05-08 11:35:52 -0600

    hw final

--- HW-Final/game.js
+++ HW-Final/game.js
@@ -17,64 +17,82 @@ const config = {
   }
 };
 
-let player, cursors, lasers, enemies, score = 0, scoreText;
-const game = new Phaser.Game(config);
+let player, cursors, spaceKey, lasers, enemies;
+let game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('background', 'images/background.jpg');
   this.load.image('player', 'images/player.png');
-  this.load.image('enemy', 'images/enemy.png');
   this.load.image('laser', 'images/laser.png');
+  this.load.image('enemy', 'images/enemy.png');
 }
 
 function create() {
   // Background
-  this.add.image(400, 300, 'background');
+  const background = this.add.image(400, 300, 'background');
+  background.setDisplaySize(this.scale.width, this.scale.height); // Full-screen background
 
   // Player
-  player = this.physics.add.sprite(400, 550, 'player').setCollideWorldBounds(true);
+  player = this.physics.add.sprite(400, 500, 'player').setScale(0.5);
+  player.setCollideWorldBounds(true);
 
   // Controls
   cursors = this.input.keyboard.createCursorKeys();
+  spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
   // Laser Group
-  lasers = this.physics.add.group();
+  lasers = this.physics.add.group({
+    defaultKey: 'laser',
+    maxSize: 10
+  });
 
-  // Enemy Group
+  // Enemies Group
   enemies = this.physics.add.group();
   spawnEnemies(this);
 
-  // Score Text
-  scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '24px', fill: '#fff' });
+  // Collisions
+  this.physics.add.overlap(lasers, enemies, destroyEnemy, null, this);
 }
 
 function update() {
+  // Player Movement
   player.setVelocityX(0);
 
-  if (cursors.left.isDown) player.setVelocityX(-300);
-  else if (cursors.right.isDown) player.setVelocityX(300);
+  if (cursors.left.isDown) {
+    player.setVelocityX(-300);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(300);
+  }
 
-  if (Phaser.Input.Keyboard.JustDown(cursors.space)) fireLaser(this);
-}
-
-function fireLaser(scene) {
-  const laser = lasers.create(player.x, player.y - 20, 'laser');
-  laser.setVelocityY(-500);
-}
-
-function spawnEnemies(scene) {
-  for (let i = 0; i < 5; i++) {
-    const enemy = enemies.create(100 + i * 120, 100, 'enemy');
-    enemy.setVelocityX(100 * (Math.random() > 0.5 ? 1 : -1));
-    enemy.setBounce(1);
-    enemy.setCollideWorldBounds(true);
+  // Fire Laser
+  if (Phaser.Input.Keyboard.JustDown(spaceKey)) {
+    fireLaser(this);
   }
 }
 
-function hitEnemy(laser, enemy) {
-  laser.destroy();
-  enemy.destroy();
+// Fire Laser Function
+function fireLaser(scene) {
+  const laser = lasers.get(player.x, player.y - 20);
+  if (laser) {
+    laser.setActive(true);
+    laser.setVisible(true);
+    laser.body.enable = true;
+    laser.setVelocityY(-500);
+  }
+}
 
-  score += 10;
-  scoreText.setText('Score: ' + score);
+// Spawn Enemies Function
+function spawnEnemies(scene) {
+  for (let i = 0; i < 5; i++) {
+    let enemy = enemies.create(100 + i * 120, 100, 'enemy');
+    enemy.setVelocityY(100);
+    enemy.setCollideWorldBounds(true);
+    enemy.setBounce(1);
+  }
+}
+
+// Destroy Enemy Function
+function destroyEnemy(laser, enemy) {
+  laser.disableBody(true, true);
+  enemy.disableBody(true, true);
 }
